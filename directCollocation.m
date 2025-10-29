a5a0f676c43e8b077ca6e8695987759be3700a26
Daniele Caradonna <danele.caradonna@gmail.com>
2025-10-29 22:26:50 +0100

    Update directCollocation.m

--- directCollocation.m
+++ directCollocation.m
@@ -89,9 +89,12 @@ P.Aineq = []; P.bineq = [];
 P.Aeq = []; P.beq = [];
 P.options = Opt.nlpOpt;
 
-% Enable Feasible Modeù
+% Enable Feasible Mode
 P.options.EnableFeasibilityMode = true;
 P.options.SubproblemAlgorithm = 'cg';
+
+% Max Iterations
+P.options.MaxIter = 5e+3;
 
 P.solver = problem.solver;
 
